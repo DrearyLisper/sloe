3cf71635cdbebbd0eedce4ae5205d65bcc70207b
Dreary Lisper <drearylisper@gmail.com>
2021-12-29 08:13:23 +0000

    Use new interface of Lambda lib.

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,25 @@ module Main where
 import System.Environment
 import Control.Monad
 
+import qualified Data.Map as Map
+
 import Sloe.Parsing (parseFile)
 import Sloe.Compilation (compile)
 import Lambda.Parsing (parseExpression, formatExpression)
 import Lambda.Evaluation (eval)
+import Lambda.Types
+
+foldF (prevDB, evals) (db, lambda) = (newDB, evaled:evals)
+  where
+    db' = Map.union prevDB db
+    (newDB, evaled) = eval (db', lambda)
+
 
 main :: IO ()
 main = do
   args <- getArgs
   let filename = head args
   content <- readFile filename
-  let expressions = parseFile content
-  forM_ expressions $ \(db, lambda) -> do
-    putStrLn $ formatExpression $ eval db lambda
+  let expressions = snd $ foldl foldF (Map.empty, []) (parseFile content)
+  forM_ (reverse expressions) $ \lambda -> do
+    putStrLn $ "> " ++ (formatExpression lambda)
